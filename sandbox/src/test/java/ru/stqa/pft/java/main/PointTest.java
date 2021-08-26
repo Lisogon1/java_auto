242d7d041aed91897f824af6c99eb56230e00925
Lisogon1 <40248753+Lisogon1@users.noreply.github.com>
2021-08-26 11:41:26 +0300

    Задание №3. Подключить тестовый фреймворк и сделать первые тесты
    2 теста для расчета расстояния между точками:
    1. Тест на корректность расчета
    2. Тест на корректность расчета, если во входящих значениях координат есть значение с типом double

---
+++ sandbox/src/test/java/ru/stqa/pft/java/main/PointTest.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.java.main;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class PointTest {
+    @Test
+    public void testDistance() {
+        Point p1 = new Point (3,5);
+        Point p2 = new Point (6,8);
+        Assert.assertEquals(p1.distanceInPoint(p2) , 4.242640687119285);
+    }
+
+    @Test
+    public void testDistanceDouble() {
+        Point p1 = new Point (0.5,0.5);
+        Point p2 = new Point (1,1);
+        Assert.assertEquals(p1.distanceInPoint(p2) , 0.7071067811865476);
+    }
+
+}
